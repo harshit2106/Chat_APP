41dd2c4603db8d5461ce464441c857b40a10003a
harshit2106 <sharsh639@gmail.com>
2022-05-04 21:21:06 +0530

    try catch

--- app.js
+++ app.js
@@ -41,7 +41,8 @@ app.use("/api/v1", userRoutes);
 app.use("/api/v1", chatRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("frontend/build"));
+  // app.use(express.static("frontend/build"));
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
 
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
